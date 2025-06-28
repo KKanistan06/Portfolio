3a28fb2782daa8007ee8c1045fddf0ba9874fcd9
Kanistan Kanesalingam <146409676+KKanistan06@users.noreply.github.com>
2025-06-28 15:21:21 +0530

    Feat: Added utils file

---
+++ src/utils/constants.js
@@ -0,0 +1,31 @@
+export const NAVIGATION_ITEMS = [
+  { name: 'Home', href: '#home', id: 'home' },
+  { name: 'About', href: '#about', id: 'about' },
+  { name: 'Projects', href: '#projects', id: 'projects' },
+  { name: 'Skills', href: '#skills', id: 'skills' },
+  { name: 'Contact', href: '#contact', id: 'contact' }
+];
+
+export const SOCIAL_LINKS = [
+  {
+    name: 'LinkedIn',
+    url: 'https://linkedin.com/in/kanistan',
+    icon: 'linkedin'
+  },
+  {
+    name: 'GitHub',
+    url: 'https://github.com/KKannistan06',
+    icon: 'github'
+  },
+  {
+    name: 'Email',
+    url: 'mailto:kkanistan874@gmail.com',
+    icon: 'email'
+  }
+];
+
+export const PERSONAL_INFO = {
+  name: 'Kanesalingam Kanistan',
+  roles: ['Software Developer', 'ML Enthusiast'],
+  email: 'kkanistan874@gmail.com'
+};
